c8ef8824b480c3700481c6abb7659c684d54484f
mrdotb <hello@mrdotb.com>
2024-03-27 09:24:46 +0100

    Fix mac script missing back quote

--- lib/lor_web/script_helpers.ex
+++ lib/lor_web/script_helpers.ex
@@ -27,7 +27,7 @@ defmodule LorWeb.ScriptHelpers do
     platform_id = get_platform_id(:mac, params.endpoint, params.platform_id)
 
     """
-    if test -d  /Applications/League\ of\ Legends.app/Contents/LoL/Game/ ; then cd /Applications/League\ of\ Legends.app/Contents/LoL/Game/ && chmod +x ./LeagueofLegends.app/Contents/MacOS/LeagueofLegends ; else cd /Applications/League\ of\ Legends.app/Contents/LoL/RADS/solutions/lol_game_client_sln/releases/ && cd $(ls -1vr -d */ | head -1) && cd deploy && chmod +x ./LeagueofLegends.app/Contents/MacOS/LeagueofLegends ; fi && riot_launched=true ./LeagueofLegends.app/Contents/MacOS/LeagueofLegends "spectator #{spectator_endpoint} #{params.encryption_key} #{params.game_id} #{platform_id}" "-UseRads" "-GameBaseDir=.."
+    if test -d  /Applications/League\\ of\\ Legends.app/Contents/LoL/Game/ ; then cd /Applications/League\\ of\\ Legends.app/Contents/LoL/Game/ && chmod +x ./LeagueofLegends.app/Contents/MacOS/LeagueofLegends ; else cd /Applications/League\\ of\\ Legends.app/Contents/LoL/RADS/solutions/lol_game_client_sln/releases/ && cd $(ls -1vr -d */ | head -1) && cd deploy && chmod +x ./LeagueofLegends.app/Contents/MacOS/LeagueofLegends ; fi && riot_launched=true ./LeagueofLegends.app/Contents/MacOS/LeagueofLegends "spectator #{spectator_endpoint} #{params.encryption_key} #{params.game_id} #{platform_id}" "-UseRads" "-GameBaseDir=.."
     """
   end
 
